2cd81e2b33e1e5e494e79a63c12e831c5fece86c
Trace Tessier <tessiert@unm.edu>
2018-09-06 20:21:40 +0000

    GuessAttempt tests passing

--- hangman/game.py
+++ hangman/game.py
@@ -2,12 +2,48 @@ from .exceptions import *
 
 
 class GuessAttempt(object):
-    pass
+
+    def __init__(self, letter, hit=False, miss=False):
+
+        # Truth values for 'hit' and 'miss' must be different
+        if hit == miss:
+            raise InvalidGuessAttempt
+
+        self.hit = hit
+        self.miss = miss
+
+    def is_hit(self):
+        return self.hit
+
+    def is_miss(self):
+        return self.miss
+
+
+
+
 
 
 class GuessWord(object):
-    pass
+
+    def __init__(self, answer):
+
+        self.answer = answer
+        self.masked = '*'*len(answer)
+
+    def perform_attempt(self, letter):
+
+        pass
 
 
 class HangmanGame(object):
-    pass
+
+    WORD_LIST = ['rmotr', 'python', 'awesome']
+
+    def __init__(self, word_list=WORD_LIST, number_of_guesses=5):
+
+        self.word_list = word_list
+        self.number_of_guesses = number_of_guesses
+
+    #@select_random_word
+    def select_random_word(cls):
+        pass
